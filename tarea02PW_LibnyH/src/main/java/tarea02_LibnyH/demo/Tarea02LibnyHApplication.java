2ac005a916e65670d117eeb2b94b871dfe21879e
Libs9924 <lhernandez90977@ufide.ac.cr>
2024-02-13 14:04:03 -0600

    paginaweb_LibnyH
    tarea02

---
+++ tarea02PW_LibnyH/src/main/java/tarea02_LibnyH/demo/Tarea02LibnyHApplication.java
@@ -0,0 +1,13 @@
+package tarea02_LibnyH.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea02LibnyHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea02LibnyHApplication.class, args);
+	}
+
+}
---
+++ tarea02PW_LibnyH/src/test/java/tarea02_LibnyH/demo/Tarea02LibnyHApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea02_LibnyH.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea02LibnyHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
